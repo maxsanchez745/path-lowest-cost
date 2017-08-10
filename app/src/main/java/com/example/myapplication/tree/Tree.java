5dff1cedc66ce67fa779f03e2e75868f5e06909f
maxsanchez745 <maxsanchez745@gmail.com>
2017-08-10 00:24:58 -0400

    Generate accumulated weights and refactored generateChildNode method

--- app/src/main/java/com/example/myapplication/tree/Tree.java
+++ app/src/main/java/com/example/myapplication/tree/Tree.java
@@ -1,5 +1,9 @@
 package com.example.myapplication.tree;
 
+import android.support.annotation.NonNull;
+
+import java.util.Arrays;
+
 /**
  * Created by evin on 8/9/17.
  */
@@ -33,15 +37,43 @@ public class Tree {
         for (TreeNode currentNode : treeNodes) {
             TreeNode[] childrenNode = new TreeNode[3]; // Up, middle, down nodes
             for (int j = 0; j < childrenNode.length; j++) {
-                childrenNode[j] = new TreeNode(currentNode.getRow() + 1, getWeight(currentNode.getRow(), column, j), currentNode.getWeight() + currentNode.getAccumulatedWeight(), null);
+                childrenNode[j] = generateChildNode(column, currentNode, j);
             }
             currentNode.setChildren(childrenNode);
             createChildren(childrenNode, column + 1);
         }
     }
 
-    private int getWeight(int row, int column, int j) {
-        return 0;
+    @NonNull
+    private TreeNode generateChildNode(int column, TreeNode currentNode, int j) {
+        final int row = currentNode.getRow();
+        final int weight = currentNode.getWeight();
+        final int accumulatedWeight = currentNode.getAccumulatedWeight();
+        final int newRow = calculateNewRow(row, j);
+        final int newWeight = getWeight(newRow, column + 1);
+        return new TreeNode(newRow, newWeight, weight + accumulatedWeight, null);
+    }
+
+    private int calculateNewRow(int row, int j) {
+        switch (j) {
+            case 0: // Up - right cell
+                if (row == 0) {
+                    return grid.length - 1; // Overflow up
+                } else {
+                    return row - 1;
+                }
+            case 2: // Bottom - right cell
+                if (row == grid.length - 1) {
+                    return 0;
+                } else {
+                    return row + 1;
+                }
+        }
+        return row;
+    }
+
+    private int getWeight(int row, int column) {
+        return grid[row][column];
     }
 
     public TreeNode[] getRootNodes() {
