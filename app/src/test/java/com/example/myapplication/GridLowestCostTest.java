68d31e5be5e3ecc89417fbab0c760ca2b4c8ecc3
maxsanchez745 <maxsanchez745@gmail.com>
2017-08-10 02:14:44 -0400

    Fix one-indexed rows bug in simple grid test case

--- app/src/test/java/com/example/myapplication/GridLowestCostTest.java
+++ app/src/test/java/com/example/myapplication/GridLowestCostTest.java
@@ -21,7 +21,7 @@ public class GridLowestCostTest {
 
         assertTrue("Simple 3x3 grid should have a path!", gridLowestCost.calculatePath());
         assertEquals("Lowest path should have weight 0!", 0, gridLowestCost.getCalculatedWeight());
-        assertArrayEquals("The path should be {1, 0, 1}!", new int[]{1, 0, 1}, gridLowestCost.getPathTaken());
+        assertArrayEquals("The path should be {2, 1, 2}!", new int[]{2, 1, 2}, gridLowestCost.getPathTaken());
     }
 
     @Test
