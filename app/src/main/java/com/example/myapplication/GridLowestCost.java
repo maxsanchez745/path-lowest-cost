4e1246299315fbdc1399a35605439a660d40ccaf
maxsanchez745 <maxsanchez745@gmail.com>
2017-08-09 21:50:34 -0400

    Add GridLowestCost contract

---
+++ app/src/main/java/com/example/myapplication/GridLowestCost.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+/**
+ * Created by evin on 8/9/17.
+ */
+
+public class GridLowestCost {
+    private static final int MAXIMUM_WEIGHT = 50;
+
+    private int[][] grid;
+
+    private boolean pathExists;
+    private int calculatedWeight;
+    private int[] pathTaken;
+
+    public GridLowestCost(int[][] grid) {
+        this.grid = grid;
+
+        pathExists = false;
+        calculatedWeight = 0;
+        pathTaken = null;
+    }
+
+    public boolean calculatePath() {
+        return false;
+    }
+
+    public int getCalculatedWeight() {
+        return calculatedWeight;
+    }
+
+    public int[] getPathTaken() {
+        return pathTaken;
+    }
+}
