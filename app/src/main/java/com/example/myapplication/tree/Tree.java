dc3557dc212e99768c58901b3ecb96fc8faa1a32
maxsanchez745 <maxsanchez745@gmail.com>
2017-08-17 00:12:33 -0400

    Add logic and implementation for early out case when there are less than 3 rows in the grid

--- app/src/main/java/com/example/myapplication/tree/Tree.java
+++ app/src/main/java/com/example/myapplication/tree/Tree.java
@@ -1,7 +1,6 @@
 package com.example.myapplication.tree;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.VisibleForTesting;
 
 /**
  * Created by user on 8/9/17.
@@ -20,15 +19,61 @@ public class Tree {
         this.grid = grid;
         this.rootNodes = new TreeNode[grid.length];
 
-        initializeRootNodes();
         if (rootNodes.length > 2) {
+            initializeRootNodes();
             createChildren(rootNodes, 0);
         } else { // Early-out case when there are either one or 2 columns
             this.rootNodes = new TreeNode[1];
             int[] lowestArray = generateLowestPathArray();
+            initializeRootNodesSimple(lowestArray[0]);
+            createChildrenSimple(rootNodes, lowestArray, 0);
         }
     }
 
+    // Initializing the root Nodes of the tree depending on the length of the grid
+    private void initializeRootNodes() {
+        for (int i = 0; i < rootNodes.length; i++) {
+            rootNodes[i] = new TreeNode(i, grid[i][0], 0, null, null);
+        }
+    }
+
+    // Initializing the root Nodes of the three with a simple value in the array using the lowest
+    // possible path (easy out since there are either only 1 or 2 rows)
+    private void initializeRootNodesSimple(int minFirstRow) {
+        rootNodes[0] = new TreeNode(minFirstRow, grid[0][minFirstRow], 0, null, null);
+    }
+
+    // Creating the children recursively using the lowestArray previously calculated, which
+    // looped through the whole grid with a early out case
+    private void createChildrenSimple(TreeNode[] rootNodes, int[] lowestArray, int column) {
+        if (column + 1 >= grid[0].length) {
+            return;
+        }
+        TreeNode parentNode = rootNodes[0];
+        TreeNode[] childrenNode = new TreeNode[1];
+        final int lowestRow = lowestArray[column];
+        childrenNode[0] = generateChildNode(column, parentNode, lowestRow, true);
+        parentNode.setChildren(childrenNode);
+        createChildrenSimple(childrenNode, lowestArray, column + 1);
+    }
+
+    // Creates the children of an array of TreeNodes depending on the column, which will keep
+    // increasing per each iteration
+    private void createChildren(TreeNode[] treeNodes, int column) {
+        if (column + 1 >= grid[0].length) {
+            return;
+        }
+        for (TreeNode currentNode : treeNodes) {
+            TreeNode[] childrenNode = new TreeNode[Math.min(3, grid.length)]; // Up, middle, down nodes
+            for (int j = 0; j < childrenNode.length; j++) {
+                childrenNode[j] = generateChildNode(column, currentNode, j, false);
+            }
+            currentNode.setChildren(childrenNode);
+            createChildren(childrenNode, column + 1);
+        }
+    }
+
+    // Creates the lowest path array in an early out case when there are only 1 or 2 rows.
     int[] generateLowestPathArray() {
         int[] lowestArray = new int[grid[0].length];
         for (int column = 0; column < grid[0].length; column++) {
@@ -44,41 +89,16 @@ public class Tree {
         return lowestArray;
     }
 
-    // Initializing the toot Nodes of the tree depending on the length of the grid
-    private void initializeRootNodes() {
-        for (int i = 0; i < rootNodes.length; i++) {
-            rootNodes[i] = new TreeNode(i, grid[i][0], 0, null, null);
-        }
-    }
-
-    private void createChildrenSimple(TreeNode[] rootNodes) {
-
-    }
-
-    // Creates the children of an array of TreeNodes depending on the column, which will keep
-    // increasing per each iteration
-    private void createChildren(TreeNode[] treeNodes, int column) {
-        if (column + 1 >= grid[0].length) {
-            return;
-        }
-        for (TreeNode currentNode : treeNodes) {
-            TreeNode[] childrenNode = new TreeNode[Math.min(3, grid.length)]; // Up, middle, down nodes
-            for (int j = 0; j < childrenNode.length; j++) {
-                childrenNode[j] = generateChildNode(column, currentNode, j);
-            }
-            currentNode.setChildren(childrenNode);
-            createChildren(childrenNode, column + 1);
-        }
-    }
-
     // Creating a new childNode based on the current column, the currentNode (which will be the
     // parent of the node created) and the
     @NonNull
-    private TreeNode generateChildNode(int column, TreeNode currentNode, int verticalPositionNextNode) {
+    private TreeNode generateChildNode(int column, TreeNode currentNode, int verticalPositionNextNode, boolean isSimple) {
         final int row = currentNode.getRow();
         final int weight = currentNode.getWeight();
         final int accumulatedWeight = currentNode.getAccumulatedWeight();
-        final int newRow = calculateNewRow(row, verticalPositionNextNode);
+        final int newRow = isSimple
+                ? verticalPositionNextNode
+                : calculateNewRow(row, verticalPositionNextNode);
         final int newWeight = getWeight(newRow, column + 1);
         return new TreeNode(newRow, newWeight, weight + accumulatedWeight, null, currentNode);
     }
